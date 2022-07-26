bf75501856826b28e00c5ef795689e25f55b9cc3
niklas1375 <niklas1375@gmail.com>
2022-07-26 15:28:43 +0200

    add required injectable annotation

--- src/app/util/custom-date-adapter.ts
+++ src/app/util/custom-date-adapter.ts
@@ -1,7 +1,9 @@
-import { NativeDateAdapter } from "@angular/material/core";
+import { Injectable } from '@angular/core';
+import { NativeDateAdapter } from '@angular/material/core';
 
+@Injectable()
 export class CustomDateAdapter extends NativeDateAdapter {
-    override getFirstDayOfWeek(): number {
-        return 1;
-    }
+  override getFirstDayOfWeek(): number {
+    return 1;
+  }
 }
